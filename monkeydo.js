348b447edac3c24d13cfe8fdd2180501390ec09b
Chris Holden <chris.holden@rainbird.ai>
2017-01-08 14:14:40 +0000

    Added more RainBird functionality for demo.

--- monkeydo.js
+++ monkeydo.js
@@ -1,3 +1,74 @@
-/**
- * Created by root on 1/6/17.
- */
+#!/usr/bin/env node
+"strict on";
+var colors = require('colors');
+
+var path = require('path');
+var fs = require('fs');
+
+var parser = require('./lib/parser');
+var executor = require('./lib/executor');
+
+var arguments = process.argv;
+
+executor.setLogExpectedErrorCallback(function(text) {
+
+    console.log(colors.magenta(text));
+    console.log('');
+
+});
+
+console.log('');
+
+if (arguments.length == 2) {
+    console.log('Please specify the script to run as the first argument.');
+    console.log('For example: monkeydo myScriptFile.txt');
+    console.log('');
+    process.exit(1);
+}
+
+var fileName = arguments[2];
+var fullFileName = path.join(process.cwd(), fileName);
+
+if (fs.existsSync(fullFileName)) {
+    console.log('Running ' + fullFileName);
+    runFile(fullFileName, function(err) {
+        console.log('');
+       if (err) {
+           process.exit(1);
+       }
+    });
+} else {
+    console.log(colors.red('There is no such file: ' + fullFileName));
+    process.exit(1);
+}
+
+function runFile(fullFileName, callback) {
+    var lines = fs.readFileSync(fullFileName).toString().split("\n");
+
+    var runnableScript = {actions: []};
+    parser.processLines(lines, runnableScript);
+
+    if (runnableScript.errors && runnableScript.errors.length > 0) {
+        console.log('The file ' + fullFileName + ' could not be loaded successfully:');
+        runnableScript.errors.forEach(function(error) {
+           console.log(colors.red(error));
+
+        });
+        callback(1);
+    }
+
+    executor.execute(runnableScript, function(err) {
+        if (err) {
+            if (err.preamble) {
+                console.log(err.preamble);
+            }
+            console.log(colors.red(err.message));
+            callback(1);
+        } else {
+            console.log(colors.green('Success.'));
+            callback();
+        }
+    });
+
+}
+
--- test/monkeydo.test.js
+++ test/monkeydo.test.js
@@ -7,22 +7,99 @@ describe('monkeydo', function() {
 
     describe('processor.processLines', function() {
 
-        it('should process the basic structures.', function(done) {
+        it ('should return an error if we call start without configuring.', function(done) {
+
             var dataPacket = {actions:[]};
 
             parser.processLines([
-                'uSe environment http://staging.rainbird.ai.',
-                'use knowledge map abc-123.',
-                'get api key from RB_API_KEY.',
                 'call start.'
             ], dataPacket);
 
             executor.execute(dataPacket, function(err) {
-                done(err);
+                expect(err).to.be.ok;
+                expect(err.action.line).to.equal(1);
+                expect(err.message).to.equal('The Url, apiKey and kmId are all required.');
+                done();
             });
-
         });
-    });
 
+        it ('should error if we expect an error and there is none.', function(done) {
+
+            var dataPacket = {actions:[]};
+
+            parser.processLines([
+                'use the api key hufflepuff.',
+                'expect error.'
+            ], dataPacket);
+
+            var errors = dataPacket.errors;
+            if (!errors) {
+                errors = [];
+            }
+            expect(errors.length).to.equal(0);
+
+            executor.execute(dataPacket, function(err) {
+                expect(err).to.be.ok;
+                expect(err.action.line).to.equal(2);
+                expect(err.action.command).to.equal('expectError');
+                expect(err.message).to.equal('The expected error did not occur.');
+                done();
+            });
+        });
+
+        it ('should not return an error if we expect it.', function(done) {
+            var dataPacket = {actions:[]};
+
+            parser.processLines([
+                'call start.',
+                'expect error.'
+            ], dataPacket);
+
+            var errors = dataPacket.errors;
+            if (!errors) {
+                errors = [];
+            }
+            expect(errors.length).to.equal(0);
+
+            executor.execute(dataPacket, function(err) {
+                expect(err).to.not.be.ok;
+                done();
+            });
+        });
+
+        it('should process the basic structures.', function(done) {
+            var dataPacket = {actions:[]};
+
+            parser.processLines([
+                'uSe environment https://staging-api.rainbird.ai.',
+                'use knowledge map bd990e1a-2d00-49af-babd-b90037be2414.',
+                'get api key from RB_API_KEY.',
+                'call start.',
+                'inject the following facts:',
+                '   bob likes cheese.'
+            ], dataPacket);
+
+            executor.execute(dataPacket, function(err) {
+                expect(err).to.be.ok;
+                expect(err.action.line).to.equal(5);
+                expect(err.message.indexOf('Could not find relationship for injection') > 0).to.be.ok;
+                done();
+            });
+        });
+
+        it('should complain if an inject line has no full stop.', function(done) {
+            var dataPacket = {actions:[]};
+
+            parser.processLines([
+                'inject the following facts:',
+                '   bob likes cheese'
+            ], dataPacket);
+
+            expect(dataPacket.errors.length).to.equal(1);
+            expect(dataPacket.errors[0].message).to.equal('All lines are expected to end with ? or . or : line: 2');
+            done();
+        });
+
+    });
 
 });
